77689ae29e274c0b71eaa4933f7401431f1c0491
Jake Faulkner <jakefaulkn@gmail.com>
2016-09-11 12:01:15 +1200

    Fix golden ratio resizing
    Used to freeze, now it doesn't

--- modules/core.el
+++ modules/core.el
@@ -19,7 +19,33 @@
     :ensure t
     :diminish (golden-ratio-mode . "")
     :config (progn
-              (add-hook 'buffer-list-update-hook #'golden-ratio)
+              (defvar golden-ratio-selected-window
+                (frame-selected-window)
+                "Selected window.")
+
+              (defun golden-ratio-set-selected-window
+                  (&optional window)
+                "Set selected window to WINDOW."
+                (setq-default
+                 golden-ratio-selected-window (or window (frame-selected-window))))
+
+              (defun golden-ratio-selected-window-p
+                  (&optional window)
+                "Return t if WINDOW is selected window."
+                (eq (or window (selected-window))
+                    (default-value 'golden-ratio-selected-window)))
+
+              (defun golden-ratio-maybe
+                  (&optional arg)
+                "Run `golden-ratio' if `golden-ratio-selected-window-p' returns nil."
+                (interactive "p")
+                (unless (golden-ratio-selected-window-p)
+                  (golden-ratio-set-selected-window)
+                  (golden-ratio arg)))
+
+              (add-hook 'buffer-list-update-hook #'golden-ratio-maybe)
+              (add-hook 'focus-in-hook           #'golden-ratio)
+              (add-hook 'focus-out-hook          #'golden-ratio)
               (golden-ratio-mode 1)))
   (use-package powerline
     :ensure t)
